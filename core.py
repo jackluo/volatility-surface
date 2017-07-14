aaab5aea6909a2d94b2621f458bcd2284a5f31fb
Jack Luo <thejackluo@gmail.com>
2017-07-14 15:49:21 -0400

    Major changes

---
+++ core.py
@@ -0,0 +1,109 @@
+# Import required libraries
+import datetime as dt
+
+import numpy as np
+import pandas as pd
+from pandas_datareader.data import Options
+from py_vollib.black_scholes_merton.implied_volatility import *
+
+from trading_calendar import USTradingCalendar
+
+
+# Get time delta
+def get_time_delta(today, date_list, trading_calendar=True):
+
+    delta_list = []
+
+    if trading_calendar:
+        year = 252
+        calendar = USTradingCalendar()
+
+        for date in date_list:
+            trading_holidays = calendar.holidays(today, date).tolist()
+            delta = np.busday_count(today, date, holidays=trading_holidays) + 1
+            delta_list.append(delta)
+    else:
+        year = 365
+
+        for date in date_list:
+            delta = abs((today - date).days) + 1
+            delta_list.append(delta)
+
+    delta_list = np.array(delta_list)
+    normalized = delta_list / float(year)
+
+    return delta_list, normalized
+
+
+# Get volatility matrix
+def get_volatility_matrix(ticker, calculate_iv=True, call=True, put=False,
+                          volume_threshold=1, above_below=False,
+                          rf_interest_rate=0.0, dividend_rate=0.0,
+                          trading_calendar=True):
+
+    if call and put:
+        raise Exception('Must specify either call or put.')
+    if not call and not put:
+        raise Exception('Must specify either call or put.')
+    if call:
+        flag = 'c'
+        typ = 'call'
+    if put:
+        flag = 'p'
+        typ = 'put'
+
+    # Fetch options data
+    tape = Options(ticker, 'yahoo')
+    data = tape.get_all_data()
+
+    if not above_below:
+        above_below = 1E9  # Very large number, good enough for our purposes
+
+    underlying = data['Underlying_Price'][0]
+
+    # Filter dataframe
+    df = data[(data.index.get_level_values('Type') == typ)
+              & (data['Vol'] >= volume_threshold)
+              & (data.index.get_level_values('Strike') < (underlying + above_below + 1))
+              & (data.index.get_level_values('Strike') > (underlying - above_below - 1))]
+
+    df
+
+    # Get columns
+    if typ == 'call':
+        premiums = df['Ask']  # Always assume user wants to get filled price
+    else:
+        preimums = df['Bid']
+
+    strikes = df.index.get_level_values('Strike').values
+    expiries = df.index.get_level_values('Expiry').to_pydatetime()
+    plotting, time_to_expirations = get_time_delta(dt.datetime.today(
+    ), expiries, trading_calendar)  # Can get slow if too many expiries
+    ivs = df['IV'].values
+
+    assert len(premiums) == len(strikes)
+    assert len(strikes) == len(time_to_expirations)
+
+    if calculate_iv:
+
+        sigmas = []
+        for premium, strike, time_to_expiration in zip(premiums, strikes, time_to_expirations):
+
+            # Constants
+            P = premium
+            S = underlying
+            K = strike
+            t = time_to_expiration
+            r = rf_interest_rate
+            q = dividend_rate
+            try:
+                sigma = implied_volatility(P, S, K, t, r, q, flag)
+                sigmas.append(sigma)
+            except:
+                sigma = 0.0
+                sigmas.append(sigma)
+
+        ivs = np.array(sigmas)
+
+    return strikes, plotting, ivs
+    print('Done')
---
+++ trading_calendar.py
@@ -0,0 +1,21 @@
+import datetime as dt
+
+from pandas.tseries.holiday import (AbstractHolidayCalendar, Holiday,
+                                    USMartinLutherKingJr, USPresidentsDay,
+                                    GoodFriday, USMemorialDay,
+                                    USLaborDay, USThanksgivingDay,
+                                    nearest_workday)
+
+
+class USTradingCalendar(AbstractHolidayCalendar):
+    rules = [
+        Holiday('NewYearsDay', month=1, day=1, observance=nearest_workday),
+        USMartinLutherKingJr,
+        USPresidentsDay,
+        GoodFriday,
+        USMemorialDay,
+        Holiday('USIndependenceDay', month=7, day=4, observance=nearest_workday),
+        USLaborDay,
+        USThanksgivingDay,
+        Holiday('Christmas', month=12, day=25, observance=nearest_workday)
+    ]
